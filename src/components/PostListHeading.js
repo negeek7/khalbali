81b4b1a33aae3976fc2cc5901cdc366489b8c6cf
Harshiiet Negi <negeek7@gmail.com>
2022-04-23 18:34:02 +0530

    header

--- src/components/PostListHeading.js
+++ src/components/PostListHeading.js
@@ -3,29 +3,31 @@ import {
   Table,
   Thead,
   Tr,
+  Box,
   Th,
   TableContainer,
   VisuallyHidden,
 } from '@chakra-ui/react';
+import postHeaderStyle from '.././styles/postHeader.module.css';
 
 function PostListHeading() {
   return (
-    <TableContainer>
-      <Table variant="simple" size="lg">
-        <Thead>
-          <Tr>
-            <Th width={4000}>Topic</Th>
-            {/* <VisuallyHidden> */}
-            <Th width={500}>Category</Th>
-            <Th>Likes</Th>
-            <Th>Replies</Th>
-            <Th>Views</Th>
-            <Th>Activity</Th>
-            {/* </VisuallyHidden> */}
-          </Tr>
-        </Thead>
-      </Table>
-    </TableContainer>
+    <div>
+      <TableContainer>
+        <Table variant="simple" size="lg">
+          <Thead>
+            <Tr>
+              <Th width={4000}>Topic</Th>
+              <Th width={500}>Category</Th>
+              <Th>Likes</Th>
+              <Th>Replies</Th>
+              <Th>Views</Th>
+              <Th>Activity</Th>
+            </Tr>
+          </Thead>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
 
